ec036245f2c3a5caafbd6744ce27f5b8c5aa2bae
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-05-01 08:52:06 +0300

    Update UserControllerTest.java

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -3,8 +3,12 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controllers.UserController;
+import ru.yandex.practicum.filmorate.exception.UserNotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+
 import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,17 +22,18 @@ class UserControllerTest {
 
     @BeforeEach
     public void beforeEach() {
-        userController = new UserController();
-        UserController.setId(1);
-        userController.users.clear();
+        final UserService userService = new UserService(new InMemoryUserStorage());
+        userController = new UserController(userService);
+        userService.setId(1);
+        userController.findAllUsers().clear();
     }
 
     @Test
     public void shouldCreateUser() {
         User user = new User(null, testEmail, testName, testLogin, testBirthday);
         userController.createUser(user);
-        assertFalse(userController.users.isEmpty());
-        assertEquals(1, userController.users.size());
+        assertFalse(userController.findAllUsers().isEmpty());
+        assertEquals(1, userController.findAllUsers().size());
     }
 
     @Test
@@ -41,7 +46,7 @@ class UserControllerTest {
                     throw new ValidationException("Электронная почта некорректна или не содержит символ @");
                 }
         );
-        assertTrue(userController.users.isEmpty());
+        assertTrue(userController.findAllUsers().isEmpty());
         User user1 = new User(null, null, testName, testLogin, testBirthday);
         assertThrows(
                 ValidationException.class,
@@ -50,7 +55,7 @@ class UserControllerTest {
                     throw new ValidationException("Электронная почта некорректна или не содержит символ @");
                 }
         );
-        assertTrue(userController.users.isEmpty());
+        assertTrue(userController.findAllUsers().isEmpty());
     }
 
     @Test
@@ -63,7 +68,7 @@ class UserControllerTest {
                     throw new ValidationException("Логин некорректен.");
                 }
         );
-        assertTrue(userController.users.isEmpty());
+        assertTrue(userController.findAllUsers().isEmpty());
         User user1 = new User(null, testEmail, testName, null, testBirthday);
         assertThrows(
                 ValidationException.class,
@@ -72,22 +77,22 @@ class UserControllerTest {
                     throw new ValidationException("Логин некорректен.");
                 }
         );
-        assertTrue(userController.users.isEmpty());
+        assertTrue(userController.findAllUsers().isEmpty());
     }
 
     @Test
     public void shouldCreateUserWithEmptyName() {
         User user = new User(null, testEmail, null, testLogin, testBirthday);
         userController.createUser(user);
-        assertFalse(userController.users.isEmpty());
-        assertEquals(1, userController.users.size());
-        assertEquals("{1=User(id=1, email=test@yandex.ru, name=lipatovKIR, login=lipatovKIR, birthday=1982-10-09)}",
-                userController.users.toString());
+        assertFalse(userController.findAllUsers().isEmpty());
+        assertEquals(1, userController.findAllUsers().size());
+        assertEquals("[User(id=1, email=test@yandex.ru, name=lipatovKIR, login=lipatovKIR, birthday=1982-10-09, friends=[])]",
+                userController.findAllUsers().toString());
     }
 
     @Test
     public void shouldNotCreateUserWithFutureBirthday() {
-        User user = new User(null, testEmail, testName, testLogin, LocalDate.of(2023,4,15));
+        User user = new User(null, testEmail, testName, testLogin, LocalDate.of(2023,6,30));
         assertThrows(
                 ValidationException.class,
                 () -> {
@@ -95,18 +100,18 @@ class UserControllerTest {
                     throw new ValidationException("Дата рождения некорректна.");
                 }
         );
-        assertTrue(userController.users.isEmpty());
+        assertTrue(userController.findAllUsers().isEmpty());
         User user1 = new User(null, testEmail, testName, testLogin, LocalDate.of(2023,4,7));
         userController.createUser(user1);
-        assertEquals(1, userController.users.size());
+        assertEquals(1, userController.findAllUsers().size());
     }
 
     @Test
     public void shouldNotUpdateUser() {
         assertThrows(
-                NullPointerException.class,
+                UserNotFoundException.class,
                 () -> {
-                    userController.updateUser(userController.users.get(6));
+                    userController.putUser(userController.findUserById("6"));
                     throw new ValidationException("Такого пользователя нет в списке.  ");
                 }
         );
@@ -120,7 +125,6 @@ class UserControllerTest {
         userController.createUser(user2);
         User user3 = new User(null, testEmail, testName, testLogin, LocalDate.of(1984,1,1));
         userController.createUser(user3);
-        assertEquals(3, userController.getAllUsers().size());
+        assertEquals(3, userController.findAllUsers().size());
     }
 }
-
