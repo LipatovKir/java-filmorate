0fa771957b581247325804ba409ec4c6e9b6aa33
LipatovKir <119127334+LipatovKir@users.noreply.github.com>
2023-05-01 09:20:13 +0300

    Update FilmControllerTest.java

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -10,13 +10,14 @@ import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
 import java.time.LocalDate;
 import java.time.Month;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerTest {
 
     private static FilmController filmController;
-    private final LocalDate correctReleaseDate = LocalDate.of(1895,  Month.DECEMBER,29);
-    private final LocalDate notCorrectReleaseDate = LocalDate.of(1895,  Month.DECEMBER,27);
+    private final LocalDate correctReleaseDate = LocalDate.of(1895, Month.DECEMBER, 29);
+    private final LocalDate notCorrectReleaseDate = LocalDate.of(1895, Month.DECEMBER, 27);
     private final String newFilmName = "Новое кино";
     private final String newFilmDescription = "Описание нового фильма";
 
@@ -25,7 +26,7 @@ class FilmControllerTest {
         final FilmService filmService = new FilmService(new InMemoryFilmStorage());
         filmController = new FilmController(filmService);
     }
-    
+
     @Test
     public void shouldCreateFilm() {
         Film film = new Film(1L, newFilmName, newFilmDescription, correctReleaseDate, 136);
@@ -37,69 +38,52 @@ class FilmControllerTest {
     @Test
     public void shouldNotCreateFilmWithEmptyName() {
         Film film = new Film(null, "  ", newFilmDescription, correctReleaseDate, 89);
-        assertThrows(
-                ValidationException.class,
-                () -> {
-                    filmController.create(film);
-                    throw new ValidationException("Нет названия фильма.");
-                }
-        );
+        assertThrows(ValidationException.class, () -> {
+            filmController.create(film);
+            throw new ValidationException("Нет названия фильма.");
+        });
         assertTrue(filmController.findAll().isEmpty());
     }
 
     @Test
     public void shouldNotCreateLongDescription() {
-        Film film = new Film(null, newFilmName, "Описание фильма очень длинное описание фильма очень длинное " +
-                "Описание фильма очень длинное Описание фильма очень длинное Описание фильма очень длинное " +
-                "Описание фильма очень длинное Описание фильма очен ", correctReleaseDate, 104);
-        assertThrows(
-                ValidationException.class,
-                () -> {
-                    filmController.create(film);
-                    throw new ValidationException("Описание фильма превышает 200 символов.");
-                }
-        );
+        Film film = new Film(null, newFilmName, "Описание фильма очень длинное описание фильма очень длинное " + "Описание фильма очень длинное Описание фильма очень длинное Описание фильма очень длинное " + "Описание фильма очень длинное Описание фильма очен ", correctReleaseDate, 104);
+        assertThrows(ValidationException.class, () -> {
+            filmController.create(film);
+            throw new ValidationException("Описание фильма превышает 200 символов.");
+        });
         assertTrue(filmController.findAll().isEmpty());
     }
 
     @Test
     public void shouldCreateLong199Description() {
-        Film film = new Film(null, newFilmName, "Описание фильма очень длинное описание фильма очень длинное " +
-                "Описание фильма очень длинное Описание фильма очень длинное Описание фильма очень длинное " +
-                "Описание фильма очень длинное Описание фильма ", correctReleaseDate, 104);
+        Film film = new Film(null, newFilmName, "Описание фильма очень длинное описание фильма очень длинное " + "Описание фильма очень длинное Описание фильма очень длинное Описание фильма очень длинное " + "Описание фильма очень длинное Описание фильма ", correctReleaseDate, 104);
         filmController.create(film);
         assertEquals(1, filmController.findAll().size());
     }
 
     @Test
     public void shouldNotCreateNotCorrectReleaseDate() {
-        Film film = new Film(null, newFilmName, newFilmDescription,
-                notCorrectReleaseDate, 113);
-        assertThrows(
-                ValidationException.class,
-                () -> {
-                    filmController.create(film);
-                    throw new ValidationException("Дата выпуска фильма не может быть раньше первого в истории человечества кинопоказа в Париже.");
-                }
-        );
+        Film film = new Film(null, newFilmName, newFilmDescription, notCorrectReleaseDate, 113);
+        assertThrows(ValidationException.class, () -> {
+            filmController.create(film);
+            throw new ValidationException("Дата выпуска фильма не может быть раньше первого в истории человечества кинопоказа в Париже.");
+        });
         assertTrue(filmController.findAll().isEmpty());
     }
 
     @Test
     public void shouldNotCreateNegativeDuration() {
         Film film = new Film(null, newFilmName, newFilmDescription, correctReleaseDate, -1);
-        assertThrows(
-                ValidationException.class,
-                () -> {
-                    filmController.create(film);
-                    throw new ValidationException("Продолжительность фильма должна быть положительной");
-                }
-        );
+        assertThrows(ValidationException.class, () -> {
+            filmController.create(film);
+            throw new ValidationException("Продолжительность фильма должна быть положительной");
+        });
         assertTrue(filmController.findAll().isEmpty());
     }
 
     @Test
-    public void shouldGetAllFilms()  {
+    public void shouldGetAllFilms() {
         Film film1 = new Film(1L, newFilmName, newFilmDescription, correctReleaseDate, 1);
         filmController.create(film1);
         Film film2 = new Film(2L, newFilmName, newFilmDescription, correctReleaseDate, 3);
@@ -107,3 +91,4 @@ class FilmControllerTest {
         assertEquals(2, filmController.findAll().size());
     }
 }
+
